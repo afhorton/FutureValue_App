1bacde8cbb8e0ccae5d082a3abc4c6df9ae732ea
afhorton <106110920+afhorton@users.noreply.github.com>
2023-06-12 22:32:25 -0600

    Uploaded complete app.

---
+++ FutureValueApp/FutureValueApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using FutureValueApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FutureValueApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        // do calculation using data collected on the form
+        public IActionResult Index(FVModel dataModel)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.FV = dataModel.CalculateFutureValue();
+            } else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(dataModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FutureValueApp/FutureValueApp/Models/FVModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FutureValueApp.Models
+{
+    public class FVModel
+    {
+        [Required(ErrorMessage ="Monthly investment is required")]
+        [Range(0, 10000, ErrorMessage ="Monthly investment value must be 0-10000")]
+        public decimal MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Annual interest rate is required")]
+        [Range(1, 20, ErrorMessage = "Annual interest rate must be 1-20")]
+        public decimal AnnualInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Years is required")]
+        [Range(1, 100, ErrorMessage = "Years must be 1-100")]
+        public int Years { get; set; }
+
+        public decimal? CalculateFutureValue()
+        {
+            decimal? futureValue = 0;
+            decimal? monthlyInterestRate = AnnualInterestRate/ 12 / 100; // as decimal not %
+            int? months = Years * 12;
+            for(int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+
+    }
+}
